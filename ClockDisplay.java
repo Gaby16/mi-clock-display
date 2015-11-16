afbe9e9c32a5b573a12d6aa7718d7d6fd535822b
Gaby <gabriel.prieto.mediavilla@gmail.com>
2015-11-16 10:54:17 +0100

    añadido constructor sin parametros

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -11,4 +11,17 @@ public class ClockDisplay
     private NumberDisplay minutes;
     private String displayString;    
     // simulates the actual display
+    
+    /**
+     * Constructor for ClockDisplay objects. This constructor 
+     * creates a new clock set at 00:00.
+     */
+    public ClockDisplay()
+    {
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        updateDisplay();
+    }
+    
+   
 }
